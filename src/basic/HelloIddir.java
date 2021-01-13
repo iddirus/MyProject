efde8a451fb7404dfa17b78904ada1e8b73d7fd7
iddirus <hadjal_d@yahoo.fr>
2021-01-13 00:43:30 -0500

    first commit to puch my code

---
+++ src/basic/HelloIddir.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class HelloIddir { // class body start
+	
+// class body: to write the code 
+	// in class we can create variable,methods
+	// a class when created has always .java extension 
+	// a code should written inside the class no outside the class
+	
+	public static void main(String[] args) { // method body start
+		
+	// to print something inside the method
+		System.out.println("welcome iddir to java class");
+		
+		System.out.println("this is iddir's training ");
+		
+		System.out.println("good morning everyone");
+		
+		// when we run our code  JVM  execute main method  
+		// inside "" double quote this is called string 
+		// we can see it in console 
+		
+		
+	} // method body end 
+	
+	
+	
+} //class body end
